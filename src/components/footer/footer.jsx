3161856e98db73f0af827e5d1331a9a8de690925
abdi mohamed <aabdymohamed@gmail.com>
2022-08-24 11:54:09 +0200

    footer commit

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function footer() {
+  return (
+    <container>
+      <form>
+
+      </form>
+     <div className="linkContainer">
+     <ul>
+        <li> <a href="Instagram"> Instagram</a> </li>
+        <li> <a href="Instagram"> Instagram</a> </li>
+        <li> <a href="Instagram"> Instagram</a> </li>
+        <li> <a href="Instagram"> Instagram</a> </li>
+        <li> <a href="Instagram"> Instagram</a> </li>
+      </ul>
+     </div>
+      
+    </container>
+  )
+}
