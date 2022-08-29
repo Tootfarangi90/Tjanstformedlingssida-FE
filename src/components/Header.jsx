91ee5cab34c8a246c34fbfd6acafce133c631528
Robert Lockett <lockettr@hotmail.com>
2022-08-29 20:21:15 +0200

    Lagt till ikoner i dashboard och fixat klart logan

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import '../css/header.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faLaptopCode, faEnvelope} from '@fortawesome/free-solid-svg-icons'
+import img from './logo.png'
+//import img from '../../public/logo.png'
 
 const Header = () => {
+
+  
+
+
   return (
     <div className="HeaderContainer">
-    <h1>AkutRaketen</h1>
-    
+      <section className='logoContainer'>
+        <img id="logo" src={img} alt="logo"/>
+      </section>
+      <section className='dashboardContainer'>
+        <FontAwesomeIcon icon={faLaptopCode} />
+        <FontAwesomeIcon icon={faEnvelope}/>
+      </section>
+      <section className='dashBoardTextContainer'>
+        <p>Login/Registrera</p>
+        <p>Meddelanden</p>
+      </section>
+      <section className='droplistContainer'>
+
+      </section>
+      
+
     </div>
   )
 }
