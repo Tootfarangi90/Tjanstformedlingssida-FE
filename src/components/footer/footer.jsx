2a5f3a7cdf6c35574e6a82ee46150b8286492658
abdi mohamed <aabdymohamed@gmail.com>
2022-08-29 13:07:37 +0200

    fuckit

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -63,7 +63,7 @@ function Footer() {
     <div className="container">
       <form ref={form} onSubmit={sendEmail}>
       <label>Name</label>
-      <input type="text" name="username" />
+      <input type="text" name="user-name" />
       <label>Email</label>
       <input type="email" name="user_email" />
       <label>Message</label>
