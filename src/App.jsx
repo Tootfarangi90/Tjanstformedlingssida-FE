eafd016d00b81065c75a9cdb0b272ceaae0bd52d
Ayda Sholani <shoolani@gmail.com>
2022-09-01 10:15:09 +0200

    removed comments in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,35 +24,6 @@ const Home = () => {
 }
 
 
-
-// const Footer = () => {
-//   const [mail, setMail] = useState("")
-//   const [subject, setSubject] = useState("")
-//   const [text, setText] = useState("")
-
-// return(
-//   <container>
-//   <form>
-//     <input type="email" name="email" placeholder='Email' />
-//     <input type="text" name="subject" placeholder='Subject' />
-//     <textarea name="text" id="" cols="30" rows="10"></textarea>
-
-//   </form>
-
-//  <div className="socialContainer">
-//  <ul>
-//     <li> <a href="Instagram"> Instagram</a> </li>
-//     <li> <a href="Instagram"> Instagram</a> </li>
-//     <li> <a href="Instagram"> Instagram</a> </li>
-//     <li> <a href="Instagram"> Instagram</a> </li>
-//     <li> <a href="Instagram"> Instagram</a> </li>
-//   </ul>
-//  </div>
-  
-// </container>
-// )
-// }
-
 function App() {
   return (
     <div className="App">
