0aa1ca9d44f995e6df8dcc46a6a09051fb239280
abdi mohamed <aabdymohamed@gmail.com>
2022-09-23 11:45:04 +0200

    test

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -35,7 +35,7 @@ const Header = (title) => {
             <li><a href="/login" className="App-link">Login</a></li>
             <li><a href="/register" className="App-link" >Register</a></li>
             <li><a href="/dashboard" className="App-link">Dashboard</a></li>
-            <li><a href="#" className="App-link">Skriv er länk</a></li>
+           
           </ul>
         </nav>
       </section>
